a56f175248c00e0dec466f9ae51b17be3b396621
limweb@hotmail.com <limweb@hotmail.com>
2024-05-18 17:42:27 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import SolanaWallets from "solana-wallets-vue";
+
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+const walletOptions = {
+  wallets: [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter({ network: "devnet" }),
+    new TorusWalletAdapter(),
+  ],
+  autoConnect: true,
+};
+
+createApp(App).use(SolanaWallets, walletOptions).mount("#app");
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from "vite";
+import vue from '@vitejs/plugin-vue'
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return defineConfig({
+    define: {
+      "process.env": env,
+    },
+    plugins: [vue()],
+    server: {
+      host: true,
+      port: 8080,
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  });
+};
